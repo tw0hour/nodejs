6a294de624cd29e723a44e4151253511877b6357
tw0hour <ismael.abdallah10@gmail.com>
2020-03-16 15:34:53 +0100

    esseayer de recuperer le port fourni par heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+  console.log('Example app listening on port ${PORT}!')
 })
+//67f34147
