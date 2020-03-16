b00fb83a377ce16e7f0c036ae752f7a95cc3c7c9
tw0hour <ismael.abdallah10@gmail.com>
2020-03-16 13:30:48 +0100

    modif server.js

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
