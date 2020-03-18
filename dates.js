ba0e2128c188b34036b4b968049e091e0f08b85e
tw0hour <ismael.abdallah10@gmail.com>
2020-03-18 13:18:45 +0100

    modif dates.js

--- dates.js
+++ dates.js
@@ -6,14 +6,11 @@ const PASSWORD = process.env.MONGODB_PASSWORD
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+
 client.connect(async err => {
   const collection = client.db("test").collection("dates");
   // affiche la liste des documents de la collection dates dans la sortie standard
   const dates = await collection.find({}).toArray();
   console.log('dates:', dates)
   client.close();
-  console.log('coucou')
 });
