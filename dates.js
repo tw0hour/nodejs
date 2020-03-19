a5d2bb9743518167c4c145227f65112fd6f13735
tw0hour <ismael.abdallah10@gmail.com>
2020-03-19 13:15:02 +0100

    correction nouvelle version

--- dates.js
+++ dates.js
@@ -1,16 +1,19 @@
-const PASSWORD = process.env.MONGODB_PASSWORD
-// il ne faut jamais stocker de mots de passe dans un dépot git, et encore moins dans un dépot git public !
-// => pour lancer ce script avec votre mot de passe, taper la commande suivante:
-// $ MONGODB_PASSWORD=<mon_mot_de_passe> node dates.js
+// => pour lancer ce script avec serveur mongodb, taper la commande suivante:
+// $ MONGODB_URI=<mon_mot_de_passe> node dates.js
+// # e.g. MONGODB_URI=mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://defaultuser:${PASSWORD}@esgi202003-ljuxa.mongodb.net/test?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(async err => {
+(async () => {
+  await client.connect();
   const collection = client.db("test").collection("dates");
   // affiche la liste des documents de la collection dates dans la sortie standard
   const dates = await collection.find({}).toArray();
   console.log('dates:', dates)
-  client.close();
-});
+
+  await collection.insertOne({ date: new Date() });
+
+  await client.close();
+})();
--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const fs = require('fs')
 const util = require('util')
 const express = require('express')
-const app = express()
+const MongoClient = require('mongodb').MongoClient;
+
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const DATABASE_NAME = 'chat-bot';
+const COLLECTION_NAME = 'messages';
+
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+
+async function readValuesFromFile() {
+  const reponses = await readFile('réponses.json', { encoding: 'utf8' })
+  return JSON.parse(reponses)
+}
+
+let collection
+const app = express();
 app.use(express.json()) // for parsing application/json
-const PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
 app.get('/hello', function (req, res) {
   const nom = req.query.nom
   if (nom) {
@@ -17,49 +32,74 @@ app.get('/hello', function (req, res) {
     res.send('Quel est votre nom ?')
   }
 })
-app.post('/chat', function (req, res) {
+
+app.get('/messages/all', async function (req, res) {
+  const messages = await collection.find({}).toArray();
+  res.send(messages)
+})
+
+app.delete('/messages/last', async function (req, res) {
+  const lastTwoDocuments = await collection.find({}).sort({ _id: -1 }).limit(2).toArray()
+  const [ botReply, userMsg ] = lastTwoDocuments
+  await collection.deleteOne(userMsg)
+  await collection.deleteOne(botReply)
+  res.send('ok')
+})
+
+app.post('/chat', async function (req, res) {
+
+  async function sendReply(reply) {
+    await collection.insertOne({ from: 'user', msg: req.body.msg })
+    await collection.insertOne({ from: 'bot', msg: reply })
+    res.send(reply)
+  }
+
   if (req.body.msg === 'ville') {
-    res.send('Nous sommes à Paris')
+    sendReply('Nous sommes à Paris')
   } else if (req.body.msg === 'météo') {
-    res.send('Il fait beau')
+    sendReply('Il fait beau')
   } else {
     if (/ = /.test(req.body.msg)) {
       const [ cle, valeur ] = req.body.msg.split(' = ')
-      readValuesFromFile()
-        .catch(err => {
-          res.send('error while reading réponses.json', err)
-        })
-        .then(valeursExistantes => {
-          const data = JSON.stringify({
-            ...valeursExistantes,
-            [cle]: valeur
-          })
-          return writeFile('réponses.json', data)
-        })
-        .then(() => {
-          res.send('Merci pour cette information !')
-        })
-        .catch((err) => {
-          console.error('error while saving réponses.json', err)
-          res.send('Il y a eu une erreur lors de l\'enregistrement')
-        })
+      let valeursExistantes
+      try {
+        valeursExistantes = await readValuesFromFile();
+      } catch (err) {
+        sendReply('error while reading réponses.json', err)
+        return
+      }
+      const data = JSON.stringify({
+        ...valeursExistantes,
+        [cle]: valeur
+      })
+      try {
+        await writeFile('réponses.json', data)
+        sendReply('Merci pour cette information !')
+      } catch (err) {
+        console.error('error while saving réponses.json', err)
+        sendReply('Il y a eu une erreur lors de l\'enregistrement')
+      }
     } else {
       const cle = req.body.msg
-      readValuesFromFile()
-        .then((values) => {
-          const reponse = values[cle]
-          res.send(cle + ': ' + reponse)
-        })
-        .catch((err) => {
-          res.send('error while reading réponses.json', err)
-        })
+      try {
+        const values = await readValuesFromFile()
+        const reponse = values[cle]
+        sendReply(cle + ': ' + reponse)
+      } catch (err) {
+        sendReply('error while reading réponses.json', err)
+      }
     }
   }
 })
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
-})
-function readValuesFromFile() {
-  return readFile('réponses.json', { encoding: 'utf8' })
-    .then(reponses => JSON.parse(reponses))
-} 
+
+;(async () => {
+  console.log(`Connecting to ${DATABASE_NAME}...`)
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  await client.connect()
+  collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME)
+  console.log(`Successfully connected to ${DATABASE_NAME}`)
+  app.listen(PORT, function () {
+    console.log('Example app listening on port ' + PORT)
+  })
+  // await client.close() // should be done when the server is going down
+})()
